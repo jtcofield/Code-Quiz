70d4c8bfb343be7882abd656135c043dd6a01297
jtcofield <jtcofield@gmail.com>
2020-10-17 20:52:00 -0400

    final code quiz app

---
+++ README.md
@@ -0,0 +1,2 @@
+# Code-Quiz
+Basic application that displays multiple choice questions in a timed quiz.  Wrong answers are penalized ten points. 
---
+++ questions.js
@@ -0,0 +1,28 @@
+// questions *****************************************************
+var questions = [
+    {
+        title: "Which of the following is not a programming language?",
+        choices: ["Stings", "Java", "Python", "HTML"],
+        answer: "Strings"
+    },
+    {
+        title: "If/Else statements are known as ______ statements.",
+        choices: ["Loop", "Function", "Conditional", "query"],
+        answer: "Conditional"
+    },
+    {
+        title: "What does prompt return?",
+        choices: ["String", "Boolean", "Array", "Number"],
+        answer: "String"
+    },
+    {
+        title: "What does confirm return?",
+        choices: ["String", "Boolean", "Array", "Number"],
+        answer: "Boolean"
+    },
+    {
+        title: "Which of the following is a semantic element?",
+        choices: ["div", "h1", "p", "all of the above"],
+        answer: "all of the above"
+    }
+]
